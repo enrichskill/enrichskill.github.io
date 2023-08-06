e87adaad1e96d67d449a5d66ed0b51acce8307ff
jiangjie <2240142544@qq.com>
2023-08-06 16:05:10 +0800

    feat:init

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,9 @@ yarn run build
 cd docs/.vuepress/dist
 
 # 输出 CNAME 文件
-echo "baomidou.com" > CNAME
-echo "www.baomidou.com" >> CNAME
-echo "google.com, pub-4147143076931995, DIRECT, f08c47fec0942fa0" > ads.txt
+#echo "baomidou.com" > CNAME
+#echo "www.baomidou.com" >> CNAME
+#echo "google.com, pub-4147143076931995, DIRECT, f08c47fec0942fa0" > ads.txt
 
 # msg="来自Github Actions的自动部署，更新于$(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S")"
 # codingUrl=https://${CODING_USER}:${CODING_TOKEN}@e.coding.net/yangyang0507/mybatis-plus-doc.git
