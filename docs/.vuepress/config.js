3a90ac82996466b51895c4bae151f099a2c0a83e
enrichskill <69141662+enrichskill@users.noreply.github.com>
2023-08-06 17:45:57 +0800

    Update config.js
    feat:url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     theme: "vdoing",
     title: "vuepress-blog",
     description: '基于vuepress 搭建的博客',
-    base: "./",
+    base: "http://http://43.139.184.41/",
     markdown: {
         lineNumbers: true
     },
