c0e4c881061a5be65b5a1450c96b2a570c47d60e
jiangjie <2240142544@qq.com>
2023-08-06 14:37:40 +0800

    init

---
+++ docs/.vuepress/config/themeConfig.js
@@ -0,0 +1,78 @@
+const nav = require("./nav.js");
+const htmlModules = require("./htmlModules.js");
+
+// Theme Config
+module.exports = {
+    nav,
+    sidebarDepth: 2,
+    logo: "/img/logo.svg",
+    repo: "baomidou/mybatis-plus",
+    searchMaxSuggestions: 10,
+    lastUpdated: "上次更新",
+
+    docsRepo: "baomidou/mybatis-plus-doc",
+    docsDir: "docs",
+    docsBranch: "master",
+    editLinks: true,
+    editLinkText: "帮助我们改善此页面！",
+
+    // Vdoing Theme Config
+    sidebar: {mode: "structuring", collapsable: true},
+
+    updateBar: {
+        showToArticle: false
+    },
+
+    category: false,
+    tag: false,
+    archive: true,
+    //pageButton: true,
+    pageStyle: 'card',
+
+    author: {
+        name: "codewhy",
+        href: "https://www.baidu.com"
+    },
+
+    social: {
+        icons: [
+            {
+                iconClass: "icon-github",
+                title: "GitHub",
+                link: "https://www.baidu.com"
+            },
+            {
+                iconClass: "icon-gitee",
+                title: "Gitee",
+                link: "https://www.baidu.com"
+            },
+            {
+                iconClass: "icon-youjian",
+                title: "发邮件",
+                link: "2240142544@qq.com"
+            }
+        ]
+    },
+
+    footer: {
+        createYear: 2023,
+        copyrightInfo: [
+            '<a href="http://baomidou.com" target="_blank" style="font-weight:bold">codewhy</a>',
+            ' | ',
+            'Sponsored by <a href="https://www.jetbrains.com" target="_blank" style="font-weight:bold">JetBrains</a>',
+            ' | ',
+            // '<a href="http://beian.miit.gov.cn/" target=_blank>渝ICP备2021000141号-1</a>',
+            '<p>友情链接：',
+            '<a href="http://aizuda.com?from=MybatisPlus" target="_blank" style="font-weight:bold">AiZuDa</a>',
+            ' | ',
+            '<a href="https://dyang.top/" target="_blank" style="font-weight:bold">D.Yang</a>',
+            ' | ',
+            '<a href="https://blog.nancheung.com/" target="_blank" style="font-weight:bold">NanCheung</a>',
+            ' | ',
+            '<a href="http://apidocs.cn/blog/" target="_blank" style="font-weight:bold">apidocsBlog</a>',
+            '</p>'
+        ].join('')
+    },
+
+    htmlModules
+};
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+home: true
+heroImage: /img/logo.png
+heroText: VuePress
+tagline: 🚀为写作开发而生
+actionText: 快速开始 →
+actionLink: https://www.baidu.com
+bannerBg: none
+
+features: # 可选的
+  - title: 润物无声
+    details: 只做增强不做改变，引入它不会对现有工程产生影响，如丝般顺滑。
+  - title: 效率至上
+    details: 只需简单配置，即可快速进行单表 CRUD 操作，从而节省大量时间。
+  - title: 丰富功能
+    details: 代码生成、自动分页、逻辑删除、自动填充等功能一应俱全。
+
+postList: node
+# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+# postList: detailed
+# simplePostListLength: 10 # 简约版文章列表显示的文章数量，默认10。（仅在postList设置为simple时生效）
+# hideRightBar: true # 是否隐藏右侧边栏
+---
+
+### 友情链接
+> [easy-es](https://easy-es.cn/) | [layui-vue](http://www.layui-vue.com/) | [jpom](https://jpom.top/) | [springdoc](https://springdoc.cn/) | [sa-token](https://sa-token.cc/)
+
