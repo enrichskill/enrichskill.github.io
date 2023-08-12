f86fe0d4f3078afbd6cb9dc0e4d3ea27e3b8eb31
jiangjie <2240142544@qq.com>
2023-08-12 10:04:09 +0800

    feat:引用位置加载

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -14,12 +14,13 @@ let sidebarArray = [
    </a>`
 ];
 
+
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
   siteData, // 站点元数据
-  isServer // 当前应用配置是处于 服务端渲染 还是 客户端
+  isServer, // 当前应用配置是处于 服务端渲染 还是 客户端
 }) => {
   // window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
   // 用于监控在路由变化时检查广告拦截器 (to主题使用者：你可以去掉本文件的所有代码)
